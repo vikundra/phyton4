abd76855653b8fdfa2bb46c2adaf56410d10d90c
vikundra <vikundra2008@gmail.com>
2023-08-06 15:08:52 +0300

    Занятие 7 ORM и Django Shell

---
+++ django3/advertisements/app_advertisements/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена',max_digits=10,decimal_places=2)
+    auction = models.BooleanField('Торг',help_text='Отметьте если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_dt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}),title={self.title},price={self.price},auction={self.auction}"
+
+    class Meta:
+        db_table = "advertisements"
